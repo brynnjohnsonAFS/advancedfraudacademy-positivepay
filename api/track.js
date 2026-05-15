85fa015b85cfd0daf0db4c296800952dff81cc1b
brynnjohnsonAFS <bjohnson@advancedfraudsolutions.com>
2026-05-14 21:24:12 -0500

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   if (req.method !== 'POST') {
@@ -26,4 +26,4 @@ export default async function handler(req, res) {
 
   console.log('[track] payload:', body, '| pardot status:', pardotStatus);
   res.status(200).json({ ok: true, pardotStatus });
-}
+};
